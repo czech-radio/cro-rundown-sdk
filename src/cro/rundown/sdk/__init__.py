6b4fe52bf36f8191436bdc6be460a178c13806eb
David Landa <david.landa@protonmail.com>
2022-04-30 18:15:54 +0200

    Fix black and isort problems

--- src/cro/rundown/sdk/__init__.py
+++ src/cro/rundown/sdk/__init__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
-
-from cro.rundown.sdk._arrange import inspect as inspect
-from cro.rundown.sdk._arrange import organize as organize
-from cro.rundown.sdk._cleanse import clean_rundown_content as clean_rundown_content
-from cro.rundown.sdk._cleanse import clean_rundown_name as clean_rundown_name
-from cro.rundown.sdk._extract import RundownParser as RundownParser
+from cro.rundown.sdk._arrange import inspect, organize
+from cro.rundown.sdk._cleanse import clean_rundown_content, clean_rundown_name
+from cro.rundown.sdk._extract import RundownParser
 
 __all__ = tuple(
     [
         "RundownParser",
+        "inspect",
+        "organize",
         "clean_rundown_name",
         "clean_rundown_content",
     ]
