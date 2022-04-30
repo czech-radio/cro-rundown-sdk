a61860ea712b1b7aaab3f7cffe2be3e786c1a799
David Landa <david.landa@protonmail.com>
2022-04-30 09:57:30 +0200

    Use black

--- tests/test_service.py
+++ tests/test_service.py
@@ -4,16 +4,19 @@
 import pytest
 
 
-
 @pytest.mark.skip
 @pytest.mark.service
 def test_rundown_cleanse():
     assert False
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 4320dcd (Update date, time, duration parsing)
+=======
+
+>>>>>>> fac1de7 (Use black)
 @pytest.mark.skip
 @pytest.mark.service
 def test_rundown_arrange():
@@ -26,6 +29,7 @@ def test_rundown_compress():
     assert False
 
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 import io
 
@@ -43,19 +47,28 @@ def rundown():
     """
     )
 =======
+=======
+import io
+
+>>>>>>> fac1de7 (Use black)
 # Parser tests.
 from cro.rundown.sdk import RundownParser
 
-import io
 
 @pytest.fixture
 def rundown():
-    return io.StringIO("""\
+    return io.StringIO(
+        """\
         <
 
 
+<<<<<<< HEAD
     """)
 >>>>>>> 4320dcd (Update date, time, duration parsing)
+=======
+    """
+    )
+>>>>>>> fac1de7 (Use black)
 
 
 @pytest.mark.skip
